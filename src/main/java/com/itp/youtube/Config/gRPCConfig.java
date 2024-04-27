a3a23d995732531eb9e984832db9b48b32ae7243
itp9177 <indutharanga@gmail.com>
2024-04-27 14:17:24 +0530

    OAuth2 security support for gRPC server

--- src/main/java/com/itp/youtube/Config/gRPCConfig.java
+++ src/main/java/com/itp/youtube/Config/gRPCConfig.java
@@ -1,22 +1,74 @@
 package com.itp.youtube.Config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import net.devh.boot.grpc.server.security.authentication.BearerAuthenticationReader;
+import net.devh.boot.grpc.server.security.authentication.CompositeGrpcAuthenticationReader;
+import net.devh.boot.grpc.server.security.authentication.GrpcAuthenticationReader;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
+import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthenticationToken;
+import org.springframework.core.convert.converter.Converter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-
-import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver;
-import org.springframework.security.web.SecurityFilterChain;
-
-
-//@Configuration
+@Configuration(proxyBeanMethods = false)
+@EnableMethodSecurity(securedEnabled = true, proxyTargetClass = true)
 public class gRPCConfig {
-   
-   private String name;
-   public gRPCConfig (){
-    //
-   }
+    private static final Logger log = LoggerFactory.getLogger(gRPCConfig.class);
+
+    final static String issuerUri = "http://0.0.0.0:8080/realms/grphql_relam/protocol/openid-connect/certs";
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        List<AuthenticationProvider> providers = new ArrayList<>();
+        JwtAuthenticationProvider jwtAuthenticationProvider = new JwtAuthenticationProvider(
+                NimbusJwtDecoder.withJwkSetUri(issuerUri).build());
+        providers.add(jwtAuthenticationProvider);
+        log.info("Jwt Authentication provider for gRPC OAuth2 jwt authentication");
+
+        return new ProviderManager(providers);
+    }
+
+    @Bean
+    GrpcAuthenticationReader authenticationReader() {
+        log.info("initiate gRCP authentication reader");
+
+        final List<GrpcAuthenticationReader> readers = new ArrayList<>();
+        readers.add(new BearerAuthenticationReader(accessToken -> new BearerTokenAuthenticationToken(accessToken)));
+        return new CompositeGrpcAuthenticationReader(readers);
+
+    }
+/*
+ * Keycloak role mapping to Springboot Granted Authorities 
+ */
+    public JwtAuthenticationConverter jwtAuthenticationConverterForKeycloak() {
+        
+        Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter = jwt -> {
+            log.debug("token", jwt);
+            Map<String, Collection<String>> realmAccess = jwt.getClaim("realm_access");
+            Collection<String> roles = realmAccess.get("roles");
+            return roles.stream()
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                    .collect(Collectors.toList());
+        };
+
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+
+        return jwtAuthenticationConverter;
+    }
 }
--- src/main/java/com/itp/youtube/grpc/ServiceImpl.java
+++ src/main/java/com/itp/youtube/grpc/ServiceImpl.java
@@ -1,26 +1,36 @@
 package com.itp.youtube.grpc;
 
-
-
+import org.springframework.security.access.annotation.Secured;
+import com.itp.youtube.Config.gRPCConfig;
 import com.itp.youtube.grpc.dto.mMessage1;
 import com.itp.youtube.grpc.dto.mMessage2.Message2;
 import com.itp.youtube.grpc.service.RouteGuideGrpc.RouteGuideImplBase;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @GrpcService
 public class ServiceImpl extends RouteGuideImplBase {
 
+    private static final Logger log = LoggerFactory.getLogger(ServiceImpl.class);
+
+  //test code
     @Override
+    @Secured("ROLE_GREET")
     public void listFeatures(Message2 request, StreamObserver<mMessage1.Message1> responseObserver) {
+    
        mMessage1.Message1 p= mMessage1.Message1.newBuilder().setFirstName("indu").setLastName("indu").build();
 
         responseObserver.onNext(p);
         responseObserver.onCompleted();
     }
-
+    
+    // test code
+    @Secured("ROLE_GREET2")
     @Override
     public void getFeature(mMessage1.Message1 request, StreamObserver<Message2> responseObserver) {
+        
         Message2 p= Message2.newBuilder().setFirstName("indu").setLastName("indu").build();
         responseObserver.onNext(p);
         responseObserver.onCompleted();
